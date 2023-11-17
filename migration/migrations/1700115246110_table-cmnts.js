d405aea4097afa8ac7548606b114873bf36dca8c
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-11-16 23:38:10 -0500

    updated

--- migration/migrations/1700115246110_table-cmnts.js
+++ migration/migrations/1700115246110_table-cmnts.js
@@ -4,18 +4,14 @@ exports.shorthands = undefined;
 
 exports.up = (pgm) => {
   pgm.sql(`
-        CREATE TABLE comments(
-            id SERIAL PRIMARY KEY
-            created_at Time stamp with time zone DEFAULT CURRENT_TIMESTAMP,
-            created_at Time stamp with time zone DEFAULT CURRENT_TIMESTAMP,
-            contents VARCHAR9250) NOT BADD
-        
-        )
+        ALTER TABLE comments 
+        RENAME COLUMN contents TO body;
     `);
 };
 
 exports.down = (pgm) => {
   pgm.sql(`
-        DROP TABLE comments
+        ALTER TABLE comments
+        RENAME COLUMN body TO contets;
     `);
 };
